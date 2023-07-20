9f3038070857bf99475cba35e0f21ed49d70218d
haruki <hinzink@foxmail.com>
2023-07-20 17:11:23 +0800

    Initial commit

---
+++ main/univariate-lstm-models.py
@@ -0,0 +1,25 @@
+from numpy import array
+# 单变量LSTM模型
+
+
+# 1.将单变量序列分解为多个样本
+# 2.基于每个样本的时间步长（此处为3），在结尾出输出单个时间步长预测值
+def split_sequence(sequence, n_steps):
+    x, y = list(), list()
+    for i in range(len(sequence)):
+        end_ix = i + n_steps
+        if end_ix > len(sequence) - 1:
+            break
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+        x.append(seq_x)
+        y.append(seq_y)
+    return array(x), array(y)
+
+
+# 样本数据
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# 选择时间步长
+n_steps = 3
+x, y = split_sequence(raw_seq, n_steps)
+for i in range(len(x)):
+    print(x[i], y[i])
---
+++ main/vanilla-lstm.py
@@ -0,0 +1,37 @@
+from numpy import array
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+
+
+# 基于单变量时间预测定义一个香草LSTM，它具有单个 LSTM 单元隐藏层和用于进行预测的输出层
+
+def split_sequence(sequence, n_steps):
+    x, y = list(), list()
+    for i in range(len(sequence)):
+        end_ix = i + n_steps
+        if end_ix > len(sequence) - 1:
+            break
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+        x.append(seq_x)
+        y.append(seq_y)
+    return array(x), array(y)
+
+
+raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+n_steps = 3
+X, y = split_sequence(raw_seq, n_steps)
+n_features = 1
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+
+model = Sequential()
+model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+
+model.fit(X, y, epochs=200, verbose=0)
+
+x_input = array([70, 80, 90])
+x_input = x_input.reshape((1, n_steps, n_features))
+yhat = model.predict(x_input, verbose=0)
+print(yhat)
