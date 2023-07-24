17b1056a75275b6829bc4a60a43364c9865eb67c
haruki <hinzink@foxmail.com>
2023-07-24 20:20:48 +0800

    07.24

--- 气象数据预测-多路并行输入和多步输出/meteo_model.py
+++ 气象数据预测-多路并行输入和多步输出/meteo_model.py
@@ -4,29 +4,42 @@ from keras.layers import LSTM
 from keras.layers import Dense
 from keras.layers import RepeatVector
 from keras.layers import TimeDistributed
+from keras.models import load_model
 import evaluation_model
 import pre_processing_data
 
-x, y, n_features, n_steps_in, n_steps_out = pre_processing_data.get_data()
+# x, y, n_features, n_steps_in, n_steps_out, scaler_out = pre_processing_data.get_data_other_day()
+x, y, n_features, n_steps_in, n_steps_out, scaler_out = pre_processing_data.get_data_same_day()
 
-model = Sequential()
-model.add(LSTM(200, activation='relu', input_shape=(n_steps_in, n_features)))
-model.add(RepeatVector(n_steps_out))
-model.add(LSTM(200, activation='relu', return_sequences=True))
-model.add(TimeDistributed(Dense(n_features)))
-model.compile(optimizer='adam', loss='mse')
+# model = Sequential()
+# model.add(LSTM(200, activation='relu', input_shape=(n_steps_in, n_features)))
+# model.add(RepeatVector(n_steps_out))
+# model.add(LSTM(200, activation='relu', return_sequences=True))
+# model.add(TimeDistributed(Dense(n_features)))
+# model.compile(optimizer='adam', loss='mse')
+#
+# model.fit(x, y, epochs=3000, verbose=0)
 
-model.fit(x, y, epochs=3000, verbose=1)
+model = load_model('model_file_v0.1.h5')
 
 x_input = x
 x_input = x_input.reshape((1, n_steps_in, n_features))
 
-yhat = model.predict(x_input, verbose=1)
+yhat = model.predict(x_input, verbose=0)
 yhat_round = np.round(yhat).astype(int)
 yhat_round_non_negative = np.clip(yhat_round, a_min=0, a_max=None)
-print([list(row) for row in yhat_round_non_negative[0]])
+yhat_round_non_negative = yhat_round_non_negative.reshape((n_steps_out, n_features))
 
+yhat_inv = scaler_out.inverse_transform(yhat_round_non_negative)
+yhat_inv = yhat_inv.reshape((n_steps_out, n_features))
+yhat_inv = np.round(yhat_inv).astype(int)
+
+yhat_inv_list = [list(row) for row in yhat_inv]
+
+print(yhat_inv_list)
 
 # 评估模型
-evaluation_model.test_view_about_model(y, yhat_round_non_negative)
-evaluation_model.evaluate(y, yhat_round_non_negative)
+evaluation_model.test_view_about_model(y, yhat)
+evaluation_model.evaluate(y, yhat)
+
+# model.save('model_file_v0.1.h5')
--- 气象数据预测-多路并行输入和多步输出/pre_processing_data.py
+++ 气象数据预测-多路并行输入和多步输出/pre_processing_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from numpy import array
+from sklearn.preprocessing import MinMaxScaler
 
 
 def split_sequences(sequences, n_steps_in, n_steps_out):
@@ -12,12 +13,20 @@ def split_sequences(sequences, n_steps_in, n_steps_out):
         seq_x, seq_y = sequences[i:end_ix, :], sequences[end_ix:out_end_ix, :]
         x.append(seq_x)
         y.append(seq_y)
+
     return array(x), array(y)
 
 
-def get_data():
+def split_other_day_sequences(input_seq, output_seq):
+    x, y = list(), list()
+    x.append(input_seq)
+    y.append(output_seq)
+    return array(x), array(y)
+
+
+def get_data_same_day():
     # 从 CSV 文件中读取数据
-    df = pd.read_csv('1_data_2023-06-29.csv')
+    df = pd.read_csv('1_data_2023-06-25.csv')
 
     # 将时间戳列转换为 DatetimeIndex 对象
     df['Time'] = pd.to_datetime(df['Time'])
@@ -32,6 +41,11 @@ def get_data():
     df_hourly = df_hourly.drop('Time', axis=1)
     data = df_hourly.to_numpy()
     data = data.astype(int)
+    print(data)
+    # 归一化处理
+    scaler_out = MinMaxScaler()
+    scaler_out.fit(data)
+    data = scaler_out.transform(data)
 
     # 拆分数据为输入和输出序列
     n_steps_in = len(data) // 2
@@ -41,4 +55,52 @@ def get_data():
 
     n_features = x.shape[2]
 
-    return x, y, n_features, n_steps_in, n_steps_out
+    return x, y, n_features, n_steps_in, n_steps_out, scaler_out
+
+
+def get_data_other_day():
+    # 从 CSV 文件中读取数据
+    df_in = pd.read_csv('1_data_2023-06-27.csv')
+    df_out = pd.read_csv('1_data_2023-06-28.csv')
+
+    # 将时间戳列转换为 DatetimeIndex 对象
+    df_in['Time'] = pd.to_datetime(df_in['Time'])
+    df_out['Time'] = pd.to_datetime(df_out['Time'])
+
+    # 将 DatetimeIndex 对象设置为索引
+    df_in = df_in.set_index('Time')
+    df_out = df_out.set_index('Time')
+
+    # 将数据按小时汇总并计算平均值
+    df_in_hourly = df_in.resample('H').mean()
+    df_out_hourly = df_out.resample('H').mean()
+    df_in_hourly = df_in_hourly.round(2)
+    df_out_hourly = df_out_hourly.round(2)
+    df_in_hourly = df_in_hourly.reset_index()
+    df_out_hourly = df_out_hourly.reset_index()
+    df_in_hourly = df_in_hourly.drop('Time', axis=1)
+    df_out_hourly = df_out_hourly.drop('Time', axis=1)
+    data_in = df_in_hourly.to_numpy()
+    data_out = df_out_hourly.to_numpy()
+    data_in = data_in.astype(int)
+    data_out = data_out.astype(int)
+
+    # 归一化处理输入数据
+    scaler_in = MinMaxScaler()
+    scaler_in.fit(data_in)
+    data_in = scaler_in.transform(data_in)
+
+    # 归一化处理输出数据
+    scaler_out = MinMaxScaler()
+    scaler_out.fit(data_out)
+    data_out = scaler_out.transform(data_out)
+
+    # 拆分数据为输入和输出序列
+    n_steps_in = len(data_in)
+    n_steps_out = len(data_out)
+
+    x, y = split_other_day_sequences(data_in, data_out)
+
+    n_features = x.shape[2]
+
+    return x, y, n_features, n_steps_in, n_steps_out, scaler_out
